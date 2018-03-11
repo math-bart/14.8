0cbb1ba454485978dc6a412aee3ae3fe0fa32355
Bartosz Wojtaś <el.klides@op.pl>
2018-03-11 18:39:40 +0100

    Change App.js

--- components/App.js
+++ components/App.js
@@ -1,13 +1,13 @@
 App = React.createClass({
-    getInitialState() {
-		return {
-			loading: false,
-			searchingText: '',
-			gif: {}
-		};
-	},
+  getInitialState() {
+    return {
+      loading: false,
+      searchingText: '',
+      gif: {}
+    };
+  },
 	
-	handleSearch: function(searchingText) {  // 1.
+  handleSearch: function(searchingText) {  // 1.
     this.setState({
       loading: true  // 2.
     });
@@ -21,42 +21,42 @@ App = React.createClass({
   },
   
   getGif: function(searchingText, callback) {  // 1.
-    var url = 'http://api.giphy.com' + '/v1/gifs/random?api_key=' + 'dc6zaTOxFJmzC' + '&tag=' + searchingText;
-	//var url = 'http://api.giphy.com/v1/gifs/search?q=' + searchingText; + 'api_key=&dc6zaTOxFJmzC&limit=5' // 2.search?q=ryan+gosling&
+    var url = 'http://api.giphy.com' + '/v1/gifs/random?api_key=' + '79d4ejI5WnmHsZqbA6TZOyeG2iB4101D' + '&tag=' + searchingText;
+    //var url = 'http://api.giphy.com/v1/gifs/search?q=' + searchingText; + 'api_key=&dc6zaTOxFJmzC&limit=5' // 2.search?q=ryan+gosling&
     var xhr = new XMLHttpRequest();  // 3.
     xhr.open('GET', url);
     xhr.onload = function() {
-        if (xhr.status === 200) {
-           var data = JSON.parse(xhr.responseText).data; // 4.
-            var gif = {  // 5.
-                url: data.fixed_width_downsampled_url,
-                sourceUrl: data.url
-            };
-            callback(gif);  // 6.
-        }
+      if (xhr.status === 200) {
+        var data = JSON.parse(xhr.responseText).data; // 4.
+        var gif = {  // 5.
+          url: data.fixed_width_downsampled_url,
+          sourceUrl: data.url
+        };
+        callback(gif);  // 6.
+      }
     };
     xhr.send();
-},
+  },
 	
-	render: function() {
+  render: function() {
 
-        var styles = {
-            margin: '0 auto',
-            textAlign: 'center',
-            width: '90%'
-        };
+    var styles = {
+      margin: '0 auto',
+      textAlign: 'center',
+      width: '90%'
+    };
 
-        return (
-          <div style={styles}>
-                <h1>Wyszukiwarka GIFow!</h1>
-                <p>Znajdź gifa na <a href='http://giphy.com'>giphy</a>. Naciskaj enter, aby pobrać kolejne gify.</p>
-                <Search onSearch={this.handleSearch}/>
-            <Gif 
-				loading={this.state.loading}
-				url={this.state.gif.url}
-				sourceUrl={this.state.gif.sourceUrl}
-			/>
-          </div>
-        );
-    }
+    return (
+      <div style={styles}>
+        <h1>Wyszukiwarka GIFow!</h1>
+        <p>Znajdź gifa na <a href='http://giphy.com'>giphy</a>. Naciskaj enter, aby pobrać kolejne gify.</p>
+        <Search onSearch={this.handleSearch}/>
+        <Gif 
+          loading={this.state.loading}
+          url={this.state.gif.url}
+          sourceUrl={this.state.gif.sourceUrl}
+        />
+      </div>
+    );
+  }
 });
