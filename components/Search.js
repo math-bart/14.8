fdb1f28ebffbe8d2595719c3cc0bbe636ff52d25
Bartosz Wojtaś <el.klides@op.pl>
2018-03-11 19:24:54 +0100

    Change App.js

--- components/Search.js
+++ components/Search.js
@@ -29,9 +29,10 @@ Search = React.createClass({
     };
 
     return <input
-      type="text"
+      type='text'
       onChange={this.handleChange}
-      placeholder="Tutaj wpisz wyszukiwaną frazę"
+	  onKeyUp={this.handleKeyUp}
+      placeholder='Tutaj wpisz wyszukiwaną frazę'
       style={styles}
       value={this.state.searchTerm}
     />
